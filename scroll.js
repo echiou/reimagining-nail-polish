b1dcec46221666912d6c61cade30fa685e55beb5
echiou <ethanchiou@berkeley.edu>
2014-01-26 16:49:12 -0800

    Navdots & Other
    Added navigation dots on right of page and JavaScript, fixed heights for
    Linux and ???.

---
+++ scroll.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    $(".sect1").click(function(){
+        $('html, body').animate({
+            scrollTop: $(".grey1").offset().top
+        }, 500);
+        $(".sect2").removeClass("current");
+        $(".sect3").removeClass("current");
+        $(".sect4").removeClass("current");
+        $(".sect1").addClass("current");
+    });
+    
+    $(".sect2").click(function(){
+        $('html, body').animate({
+            scrollTop: $(".turq").offset().top
+        }, 500);
+        $(".sect1").removeClass("current");
+        $(".sect3").removeClass("current");
+        $(".sect4").removeClass("current");
+        $(".sect2").addClass("current");
+    });
+    
+    $(".sect3").click(function(){
+        $('html, body').animate({
+            scrollTop: $(".grey2").offset().top
+        }, 500);
+        $(".sect1").removeClass("current");
+        $(".sect2").removeClass("current");
+        $(".sect4").removeClass("current");
+        $(".sect3").addClass("current");
+    });
+    
+    $(".sect4").click(function(){
+        $('html, body').animate({
+            scrollTop: $(".pink").offset().top
+        }, 500);
+        $(".sect1").removeClass("current");
+        $(".sect2").removeClass("current");
+        $(".sect3").removeClass("current");
+        $(".sect4").addClass("current");
+    });
+
+    $(document).scroll(function(){
+        if($(this).scrollTop() < 1060)
+        {   
+            $(".sect2").removeClass("current");
+            $(".sect3").removeClass("current");
+            $(".sect4").removeClass("current");
+            $(".sect1").addClass("current");
+        }
+        if($(this).scrollTop() > 1060 && $(this).scrollTop() < 2184)
+        {   
+            $(".sect1").removeClass("current");
+            $(".sect3").removeClass("current");
+            $(".sect4").removeClass("current");
+            $(".sect2").addClass("current");
+        }
+        if($(this).scrollTop() > 2184 && $(this).scrollTop() < 2642)
+        {   
+            $(".sect1").removeClass("current");
+            $(".sect2").removeClass("current");
+            $(".sect4").removeClass("current");
+            $(".sect3").addClass("current");
+        }
+        if($(this).scrollTop() > 2642 && $(this).scrollTop() < 3000 || $(window).scrollTop() + $(window).height() == $(document).height())
+        {   
+            $(".sect1").removeClass("current");
+            $(".sect2").removeClass("current");
+            $(".sect3").removeClass("current");
+            $(".sect4").addClass("current");
+        }
+    });
+});
