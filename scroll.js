2ea5b03eff00ac7bf51e98a8851e2c33a5ba4a4b
Ethan Chiou <ethanchiou@berkeley.edu>
2015-10-15 12:22:09 -0700

    fix scroll, change index link

--- scroll.js
+++ scroll.js
@@ -1,72 +1,62 @@
 $(document).ready(function(){
+    updateDots();
+
     $(".sect1").click(function(){
         $('html, body').animate({
             scrollTop: $(".grey1").offset().top
         }, 500);
-        $(".sect2").removeClass("current");
-        $(".sect3").removeClass("current");
-        $(".sect4").removeClass("current");
-        $(".sect1").addClass("current");
     });
-    
+
     $(".sect2").click(function(){
         $('html, body').animate({
             scrollTop: $(".turq").offset().top
         }, 500);
-        $(".sect1").removeClass("current");
-        $(".sect3").removeClass("current");
-        $(".sect4").removeClass("current");
-        $(".sect2").addClass("current");
     });
-    
+
     $(".sect3").click(function(){
         $('html, body').animate({
             scrollTop: $(".grey2").offset().top
         }, 500);
-        $(".sect1").removeClass("current");
-        $(".sect2").removeClass("current");
-        $(".sect4").removeClass("current");
-        $(".sect3").addClass("current");
     });
-    
+
     $(".sect4").click(function(){
         $('html, body').animate({
             scrollTop: $(".pink").offset().top
         }, 500);
-        $(".sect1").removeClass("current");
-        $(".sect2").removeClass("current");
-        $(".sect3").removeClass("current");
-        $(".sect4").addClass("current");
     });
 
     $(document).scroll(function(){
-        if($(this).scrollTop() < 1060)
-        {   
+        updateDots();
+    });
+
+    function updateDots() {
+        if($(this).scrollTop() < 1059)
+        {
             $(".sect2").removeClass("current");
             $(".sect3").removeClass("current");
             $(".sect4").removeClass("current");
             $(".sect1").addClass("current");
         }
-        if($(this).scrollTop() > 1060 && $(this).scrollTop() < 2184)
-        {   
+        if($(this).scrollTop() > 1059 && $(this).scrollTop() < 2183)
+        {
             $(".sect1").removeClass("current");
             $(".sect3").removeClass("current");
             $(".sect4").removeClass("current");
             $(".sect2").addClass("current");
         }
-        if($(this).scrollTop() > 2184 && $(this).scrollTop() < 2642)
-        {   
+        if($(this).scrollTop() > 2183 && $(this).scrollTop() < 2641)
+        {
             $(".sect1").removeClass("current");
             $(".sect2").removeClass("current");
             $(".sect4").removeClass("current");
             $(".sect3").addClass("current");
         }
-        if($(this).scrollTop() > 2642 && $(this).scrollTop() < 3000 || $(window).scrollTop() + $(window).height() == $(document).height())
-        {   
+        if($(this).scrollTop() > 2641 && $(this).scrollTop() < 3000 || $(window).scrollTop() + $(window).height() == $(document).height())
+        {
             $(".sect1").removeClass("current");
             $(".sect2").removeClass("current");
             $(".sect3").removeClass("current");
             $(".sect4").addClass("current");
         }
-    });
+    }
 });
